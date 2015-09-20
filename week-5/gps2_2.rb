7f009d694ae79a6e058e43c7657a560c771547ed
Awilda <awildacantres@gmail.com>
2015-09-20 14:19:10 -0400

    Add reflection

--- week-5/gps2_2.rb
+++ week-5/gps2_2.rb
@@ -59,9 +59,32 @@ p test
 
 =begin
 What did you learn about pseudocode from working on this challenge?
+I learned to be very thorough with my pseudocode. I had some trouble with the directions
+and didn't realize that I was going to define 4 methods. In this case, I learned that writing
+separate pseudocodes for each (aside from initialize) method would be the best option and would
+prove to be quite effective.
+
 What are the tradeoffs of using Arrays and Hashes for this challenge?
+I decided to use Hashes for this challenge since I could make the item a key and the quantity a
+value. It would make it easier to make the quantity changes when calling on the key of the hash.
+If an Array had been used, I think every new item and quatity would probably be a subarray and I'd
+have to modify within that.
+
 What does a method return?
+A method returns the the "do-something" code. In this case, the method takes in the arguments and
+makes them act a certain way.
+
 What kind of things can you pass into methods as arguments?
+You can pass strings, arrays and numbers as arguments
+
 How can you pass information between methods?
+You can pass information bewtween methods using instance variables. It's written the same but with
+a "@" sign in front such as: @instance_method. Multiple methods can also have the same arguments
+which also allow you to you those objects within several methods.
+
 What concepts were solidified in this challenge, and what concepts are still confusing?
+I was definitely able to learn more about Hashes in this challenge. I also learned that I didn't have
+to assign the arguments to individual instance variables since I would already be assigning them to
+the @list instance variable I created for an empty hash. Then, from there I could do the adjustments
+and modifications required in the challenge. 
 =end
