1a82c26d2a25b76fbad6e2d82df956d361ec91ac
Awilda <awildacantres@gmail.com>
2015-09-30 22:46:06 -0400

    Add initial solution

--- week-7/nums_commas.js
+++ week-7/nums_commas.js
@@ -8,6 +8,28 @@
 
 
 // Initial Solution
+function commas(integer) {
+    if (integer < 1000) {
+      return integer;
+    }
+    else {
+      var idx = -3;
+      var count = 0;
+      var arr = integer.toString().split("");
+      while (-idx < integer.toString().length + count) {
+      arr.splice(idx, 0, ",");
+      idx -= 4;
+      count++
+    };
+      return arr.join("");
+  };
+};
+
+
+var int = 100100100
+
+console.log(commas(int));
+
 
 
 
